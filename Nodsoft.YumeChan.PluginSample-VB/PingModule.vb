adb6fbc1a9b7ce09eeae910d80785dbfe587417f
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-11-20 09:45:59 +0100

    Create Ping Module

---
+++ Nodsoft.YumeChan.PluginSample-VB/PingModule.vb
@@ -0,0 +1,23 @@
+﻿Imports Discord
+Imports Discord.Commands
+
+
+' Classes can here act as a bundle of Commands. The only limitation is one Class per Group Name.
+'
+' A few Rules :
+'   - Class MUST inherit ModuleBase to get flagged as a Module containing commands.
+'   - Class MUST be set as public to get picked up by the Loader.
+'
+' /!\ DO NOT USE THESE CLASSES TO STORE PERSISTENT DATA. /!\
+' Each "Command Group" Class is Instantiated when a Command from its group is called. 
+' The instance immediately vanishes after finishing command execution.
+<Group("ping")>
+Public Class PingModule
+    Inherits ModuleBase(Of SocketCommandContext)
+
+    <Command("visualbasic"), [Alias]("vb")>
+    Public Async Function PingHelloVisualBasicCommandAsync() As Task
+        Await ReplyAsync("Hello from YumeChan's Visual Basic Sample Plugin !")
+    End Function
+
+End Class
